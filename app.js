8a081d2096621b83810bea153554eaf0290cf280
Aleksey Kholmogorov <alexholm222@yandex.ru>
2022-06-29 18:17:37 +0300

    промежуточный

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true
+});
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '62bc5121795cf342f3676736'
+  };
+
+  next();
+});
+
+app.use('/users', require('./routes/users'));
+app.use('/cards', require('./routes/cards'));
+
+app.listen(PORT)
---
+++ models/card.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const cardShema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
+  },
+  likes: {
+    default: []
+  },
+  createdAd: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('card', cardShema)
