7488c932a7d6d6b7d867b7023c1be87fd6aba21b
Aleksey Kholmogorov <alexholm222@yandex.ru>
2022-07-01 23:29:05 +0300

    ПР13/fix: Исправление ошибок по тестам

--- app.js
+++ app.js
@@ -22,5 +22,7 @@ app.use((req, res, next) => {
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
-
+app.use((req, res) => {
+  res.status(404).send({message: "404 - страница не найдена"})
+})
 app.listen(PORT)
