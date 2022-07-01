ec61e002e9a7cc46d09e6dfa9497b2b874463d89
Aleksey Kholmogorov <alexholm222@yandex.ru>
2022-07-01 22:51:09 +0300

    ПР13/fix: Исправление ошибок тестов

--- app.js
+++ app.js
@@ -22,7 +22,5 @@ app.use((req, res, next) => {
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
-app.use('*', (req, res, next) => next(
-  new Error('Запрошен не существующий ресурс'),
-));
+
 app.listen(PORT)
